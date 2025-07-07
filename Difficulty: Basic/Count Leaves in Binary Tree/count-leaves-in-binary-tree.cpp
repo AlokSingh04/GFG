d7380506a3a766670929a2983bc330ac50eaffc4
Alok Singh <140353318+AlokSingh04@users.noreply.github.com>
2025-07-07 10:39:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Leaves in Binary Tree/count-leaves-in-binary-tree.cpp
@@ -0,0 +1,28 @@
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+
+// Class Solution
+class Solution {
+  public:
+    void countLeaf(Node* root, int& count){
+        if(root == nullptr) return;
+        
+        if(!root->left && !root->right) count++;
+        
+        countLeaf(root->left,count);
+        countLeaf(root->right,count);
+    }
+    // Function to count the number of leaf nodes in a binary tree.
+    int countLeaves(Node* root) {
+        int count = 0;
+        countLeaf(root,count);
+        
+        return count;
+    }
+};

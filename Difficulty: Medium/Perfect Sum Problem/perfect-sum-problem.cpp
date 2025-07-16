ae8915327ac28322108b0f4604067e9413bc94a1
Alok Singh <140353318+AlokSingh04@users.noreply.github.com>
2025-07-16 12:16:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -4,9 +4,9 @@ class Solution {
         int n = arr.size();
         vector<vector<int>> dp(n+1,vector<int>(target+1,0));
         
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 1;
-        }
+        // for(int i=0;i<=n;i++){
+            dp[0][0] = 1;
+        // }
         
         for(int i=1;i<=n;i++){
             for(int j=0;j<=target;j++){

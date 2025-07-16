582e69751ba1d44156d38ca5dbc69cb608988c77
Alok Singh <140353318+AlokSingh04@users.noreply.github.com>
2025-07-16 12:15:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -5,7 +5,7 @@ class Solution {
         vector<vector<int>> dp(n+1,vector<int>(W+1,0));
         
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=W; j++){
+            for(int j=0; j<=W; j++){
                 if(wt[i-1] > j) dp[i][j] = dp[i-1][j];
                 else dp[i][j] = max(dp[i-1][j], val[i-1] + dp[i-1][j-wt[i-1]]);
             }

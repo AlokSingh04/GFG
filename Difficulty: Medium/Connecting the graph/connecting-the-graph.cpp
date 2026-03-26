a8fbd5d98a83dd7e5b536e3fc87b2b3aa38354ea
Alok Singh <140353318+AlokSingh04@users.noreply.github.com>
2026-03-26 15:23:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Connecting the graph/connecting-the-graph.cpp
@@ -0,0 +1,38 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+        visited[node] = true;
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adj, visited);
+            }
+        }
+    }
+  
+    int Solve(int n, vector<vector<int>>& edges) {
+        // code here
+        if (edges.size() < n - 1) {
+            return -1;
+        }
+
+        vector<vector<int>> adj(n);
+        for (vector<int>& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+        vector<bool> visited(n, false);
+        int components = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                components++;
+                dfs(i, adj, visited);
+            }
+        }
+
+        return components - 1;
+    }
+};
